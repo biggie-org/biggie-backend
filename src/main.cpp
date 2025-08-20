f66ee15502320873183e2dd61f5764819ef0ff6d
jerious <jerious_withoutvalue@outlook.com>
2025-08-19 23:20:12 -0300

    updated main method (idk, i like void main)

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,7 @@
 /**
  * Sim, eu odeio C++, mas ao mesmo tempo, eu amo as features, principalmente strings.
  */
-int main()
+int main(void)
 {
 	Socket::InitServer("127.0.0.1", 8080);
-
-	return 0;
 }
